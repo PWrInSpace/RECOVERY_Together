2f44ffcf9b90b86117d56b3a11f028c255880a06
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-07-26 23:14:23 +0200

    gitignore and removed some pins from config

--- components/config/config.h
+++ components/config/config.h
@@ -1,11 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/***************************  ESP DEFINE  ************************************/
-
- #define ESP_LEFT
- //#define ESP_RIGHT
-
 /************************ COMMANDS FROM MCB **********************************/
 
 #define EASYMINI_ARM_CMD 0x01
@@ -14,11 +9,6 @@
 #define TELEMETRUM_DISARM_CMD 0x04
 #define FORCE_FIRST_STAGE_CMD 0xa5
 #define FORCE_SECOND_STAGE_CMD 0x5a
-
-/**************************  ARMING STATUS  ***********************************/
-
-#define DISARMED 0x00
-#define ARMED 0x01
 
 /***************************  COTS STATES  ************************************/
 
@@ -31,14 +21,6 @@
 #define RET_SUCCESS 0x00
 #define RET_FAILTURE 0x01
 
-#ifdef ESP_LEFT
-#define DEV_ADR 0x0B
-#endif
-
-#ifdef ESP_RIGHT
-#define DEV_ADR 0x0A
-#endif
-
 #define I2C_SLAVE_PORT I2C_NUM_0
 #define I2C_SLAVE_TX_BUF_LEN 256
 #define I2C_SLAVE_RX_BUF_LEN 256
@@ -46,6 +28,5 @@
 #define MCB_ADR 0x00
 #define DATA_SIZE_FROM_MCB 8
 #define DATA_SIZE_TO_MCB 4
-
 
 #endif //  CONFIG_H
