4009bd62452be7c4a869919d90c3be721270693e
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-08-04 17:48:24 +0200

    spi config

--- components/app/app.c
+++ components/app/app.c
@@ -1,4 +1,11 @@
 #include "app.h"
+#include "app_state.h"
+
+static app_state_t current_state;
+
+app_state_t* app_get_state(void) {
+    return &current_state;
+}
 
 esp_err_t app_init(void) {
     telemetrum_init();
