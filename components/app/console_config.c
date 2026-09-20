0e85b89308be892e257e64e6076a8682c231a98c
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-09-20 15:51:44 +0200

    added print state

--- components/app/console_config.c
+++ components/app/console_config.c
@@ -78,6 +78,30 @@ static int easymini_disarm_cmd(int argc, char** argv) {
     return 0;
 }
 
+static int print_state(int argc, char** argv) {
+    ESP_LOGI(TAG, "========== RECOVERY STATE ==========");
+    ESP_LOGI(TAG, "Recovery:");
+    ESP_LOGI(TAG, "  Separation 1:    %s", recovery.data.separation_one ? "DONE" : "WAITING");
+    ESP_LOGI(TAG, "  Separation 2:    %s", recovery.data.separation_two ? "DONE" : "WAITING");
+    ESP_LOGI(TAG, "------------------------------------");
+    ESP_LOGI(TAG, "Telemetrum:");
+    ESP_LOGI(TAG, "  Armed:           %s", telemetrum.data.armed ? "YES" : "NO");
+    ESP_LOGI(TAG, "  Apogee Detected: %s", telemetrum.data.apogee_detected ? "YES" : "NO");
+    ESP_LOGI(TAG, "  First Stage:     %s", telemetrum.data.first_stage ? "DEPLOYED" : "WAITING");
+    ESP_LOGI(TAG, "  Second Stage:    %s", telemetrum.data.second_stage ? "DEPLOYED" : "WAITING");
+    ESP_LOGI(TAG, "------------------------------------");
+    ESP_LOGI(TAG, "EasyMini:");
+    ESP_LOGI(TAG, "  Armed:           %s", easymini.data.armed ? "YES" : "NO");
+    ESP_LOGI(TAG, "  Apogee Detected: %s", easymini.data.apogee_detected ? "YES" : "NO");
+    ESP_LOGI(TAG, "  First Stage:     %s", easymini.data.first_stage ? "DEPLOYED" : "WAITING");
+    ESP_LOGI(TAG, "  Second Stage:    %s", easymini.data.second_stage ? "DEPLOYED" : "WAITING");
+    ESP_LOGI(TAG, "------------------------------------");
+    ESP_LOGI(TAG, "Servo:");
+    ESP_LOGI(TAG, "  Current Angle:   %d", servo.angle);
+    ESP_LOGI(TAG, "====================================");
+    return 0;
+}
+
 static esp_console_cmd_t cmd[] = {
     {"servo-open", "Open servo", NULL, servo_open_cmd, NULL, NULL, NULL},
     {"servo-close", "Close servo", NULL, servo_close_cmd, NULL, NULL, NULL},
@@ -91,6 +115,7 @@ static esp_console_cmd_t cmd[] = {
     {"telemetrum-disarm", "Disarm Telemetrum", NULL, telemetrum_disarm_cmd, NULL, NULL, NULL},
     {"easymini-arm", "Arm EasyMini", NULL, easymini_arm_cmd, NULL, NULL, NULL},
     {"easymini-disarm", "Disarm EasyMini", NULL, easymini_disarm_cmd, NULL, NULL, NULL},
+    {"print-state", "Print state of the system", NULL, print_state, NULL, NULL, NULL},
 };
 
 esp_err_t init_console(void) {
